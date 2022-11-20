56cd109c69e2616742a1ef3fcf63c7e2efa751e6
Volodymyr Butyter <butytervolodia@gmail.com>
2022-11-20 13:38:05 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const createdGalleryEl = createEl(galleryItems);
+
+galleryEl.innerHTML = createdGalleryEl;
+
+function createEl(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    }).join("");
+}
+
