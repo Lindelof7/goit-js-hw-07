ecd5576f8c75f8983ec523239b988b4c6a6e83ee
Volodymyr Butyter <butytervolodia@gmail.com>
2022-11-20 14:49:37 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,6 +6,7 @@ console.log(galleryItems);
 const galleryEl = document.querySelector('.gallery');
 const createdGalleryEl = createEl(galleryItems);
 
+
 galleryEl.innerHTML = createdGalleryEl;
 
 function createEl(galleryItems) {
@@ -22,5 +23,34 @@ function createEl(galleryItems) {
   </a>
 </div>`;
     }).join("");
+}
+
+galleryEl.addEventListener("click", onImgClick)
+
+function onImgClick(e) {
+  e.preventDefault()
+  const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${e.target.dataset.source}" width = "800" height = "600">
+    </div>
+`)
+
+   if (e.target.nodeName !== 'IMG') {
+    return
+  } else {
+  
+instance.show()
+   }
+  if (instance.show()) {
+  document.addEventListener("keydown",(e)=>
+    {
+      if(e.key === "Escape"){
+    instance.close()
+    } 
+})
+  }
+  if (e.key === "Escape") {
+    document.removeEventListener("keydown", (e)  )
+  }
 }
 
