4c128ef0fe8d8bb434a405d380af6b012a3a959b
Volodymyr Butyter <butytervolodia@gmail.com>
2022-11-20 19:10:16 +0200

    homework 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const createdGalleryEl = createEl(galleryItems);
+
+
+function createEl(galleryItems) {
+    return   galleryItems.map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join('')
+}
+
+galleryEl.innerHTML = createdGalleryEl;
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+    captionsData: 'alt',
+    captiondDelay: 250
+ })
